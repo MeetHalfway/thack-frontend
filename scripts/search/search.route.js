a388c2c1754462b7986536815290e07d063829f5
oleg <oleg@qoalu.com>
2016-03-05 20:06:06 +0100

    creating route

--- scripts/search/search.route.js
+++ scripts/search/search.route.js
@@ -3,7 +3,7 @@
     'use strict';
 
     angular
-        .module('app.index')
+        .module('app.search')
         .config(routerConfig);
 
     function routerConfig($stateProvider, $urlRouterProvider) {
@@ -12,33 +12,6 @@
         /**
          * Resolve global application-wide data at here
          */
-        $stateProvider.state('root', {
-            abstract: true,
-            template: '<div ui-view></div>',
-            resolve: {
-                globalConfig: function () {
-                    return {};
-                }
-            }
-        });
-
-        //        $stateProvider.state('index', {
-        //            url: '/',
-        //            parent: 'root',
-        //            views: {
-        //                '': {
-        //                    templateUrl: 'templates/index/index.html',
-        //                    controller: 'indexCtrl'
-        //                },
-        //                'search@index': {
-        //                    templateUrl: 'templates/search/search.html',
-        //                    controller: 'indexCtrl',
-        //                    resolve: {
-        //                        searchFormData: getDefaultData
-        //                    }
-        //                }
-        //            }
-        //        });
 
         $stateProvider.state('intro', {
             url: '/',
@@ -56,7 +29,7 @@
             templateUrl: 'templates/intro/friendlist.html',
             controller: 'introCtrl',
             resolve: {
-                //users: loadUsers
+                users: loadUsers
             }
         });
 
@@ -79,5 +52,9 @@
             origin: null,
             date: null
         }
+    }
+
+    function getFriendlist() {
+        retur
     }
 })();

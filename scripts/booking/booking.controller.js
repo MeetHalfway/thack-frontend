26a3029df82a4b7017ee76b96ccec8efb77508c5
Sascha Gros <gros.sascha@gmail.com>
2016-03-06 12:48:05 +0100

    Improve freaking times in booking module

--- scripts/booking/booking.controller.js
+++ scripts/booking/booking.controller.js
@@ -25,6 +25,32 @@
             $state.go('search')
         };
 
+        $scope.getArrivalTime = function(trip, outbound, peopleIndex) {
+
+            var departureTime = outbound ? trip.flights[peopleIndex].indate :
+                trip.flights[peopleIndex].outdate;
+
+            var duration = outbound ? trip.flights[peopleIndex].induration :
+                trip.flights[peopleIndex].outduration;
+
+            departureTime = moment(departureTime, "YYYY-MM-DDThh:mm");
+            duration = moment.duration(duration, 'minutes');
+
+            var arrivalTime = departureTime.clone().add(duration);
+
+            return arrivalTime.format("H:mm");
+        };
+
+        $scope.getDepartureTime = function(trip, outbound, peopleIndex) {
+
+            var departureTime = outbound ? trip.flights[peopleIndex].indate :
+                trip.flights[peopleIndex].outdate;
+
+            departureTime = moment(departureTime, "YYYY-MM-DDThh:mm");
+
+            return departureTime.format("H:mm");
+        };
+
         /*
         $scope.trip = function(trip) {
             $state.go('booking.flights', { 'trip' : JSON.stringify(trip) });
